33fc12a16faaa75e7175e16ebda70ab0027e9e12
Cold-Mint <cold_mint@qq.com>
2026-05-27 11:22:30 +0800

    No longer generate toml methods for objects without any member variables. Enhance the parsing ability for C++ structure declarations.

--- main.go
+++ main.go
@@ -3,5 +3,8 @@ package main
 import "GlimmerWorksCli/cmd"
 
 func main() {
+	//When you need to manually test the command, edit this line of code.
+	//当你需要手动测试命令时编辑这行代码。
+	//os.Args = []string{"GlimmerWorksCli", "genCode", "-t", "2", "-d", "/home/coldmint/projects/GlimmerWorks", "-o", "/home/coldmint/projects/GlimmerWorks/core/utils"}
 	cmd.Execute()
 }
